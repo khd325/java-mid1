980338b654b273067daff814f8c3e6887634cabd
khd325 <hdk325@naver.com>
2024-09-21 20:59:53 +0900

    String 클래스

---
+++ src/lang/string/test/TestString3.java
@@ -0,0 +1,11 @@
+package lang.string.test;
+
+public class TestString3 {
+
+    public static void main(String[] args) {
+        String str = "hello.txt";
+
+        int index = str.indexOf(".");
+        System.out.println("index = " + index);
+    }
+}
