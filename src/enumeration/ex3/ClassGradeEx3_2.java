10cbc1cbea78c41264c7f296f3a99ff9e4cee6db
khd325 <hdk325@naver.com>
2024-09-28 17:03:01 +0900

    열거형 - ENUM

---
+++ src/enumeration/ex3/ClassGradeEx3_2.java
@@ -0,0 +1,18 @@
+package enumeration.ex3;
+
+
+public class ClassGradeEx3_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        /*
+        Grade grade = new Grade();
+        int result = discountService.discount(grade, price);
+        System.out.println("result price: " + result);
+        */
+
+    }
+}
